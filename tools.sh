d3e4383748384a6e5a87a348581225c839b2603e
lijun.zhou <zhouj.172@gmail.com>
2019-06-18 18:10:43 +0800

    rfmaven

--- tools.sh
+++ tools.sh
@@ -120,8 +120,11 @@ initAndUpdateNulsRuntime()
     fi
     if [ -x "mvn" ]; then
         if [ ! -d "./lib/maven" ]; then
-            tar -xvf ./lib/apache-maven-3.6.1-bin.tar.gz
+            cd ./lib
+            wget http://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
+            tar -xvf ./apache-maven-3.6.1-bin.tar.gz
             mv ./apache-maven-3.6.1 ./maven
+            cd ..
         fi
         path=`pwd`
         export MAVEN_HOME=${path}/lib/maven
